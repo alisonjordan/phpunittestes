bbcab0566b364a97fe615a727457cdf34451bc8f
jpedromcp <jppedromonteiro11@gmail.com>
2023-06-14 13:47:22 -0300

    Apagando script de test

--- teste.php
+++ teste.php
@@ -1,20 +1,2 @@
 <?php
 
-
-//include 'src/Dado.php';
-include 'src/Craps.php';
-
-//$dado1 = new Dado();
-//$dado2 = new Dado();
-$craps = new Craps();
-
-/*
-$craps->setDados($dado1, $dado2);
-$craps->lancarDados();
-$craps->getDadosFacesSorteadas();
-*/
-
-
-echo PHP_EOL;
-
-$craps->play();
